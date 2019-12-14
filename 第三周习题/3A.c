f28f8db0d02507c7357c2b3d28f34d9bf65afd64
NiGo <1275810355@qq.com>
2019-12-14 21:44:38 +0800

    3A

--- 第三周习题/3A.c
+++ 第三周习题/3A.c
@@ -1,15 +1,9 @@
-/*************************************************************************
-    > File Name: 3A.c
-    > Author: zll
-    > Mail: zhnllion@126.com 
-    > Created Time: 2019年12月11日 星期三 19时34分51秒
- ************************************************************************/
 //大数加法
 #include<stdio.h>
 #include<string.h>
 int main()
 {
-	char A[500],B[500];
+	char A[505],B[505];
 	int lenA,lenB,maxlen;
 	int i,j;
 	while(scanf("%s %s",A,B)!=EOF)
@@ -20,7 +14,7 @@ int main()
 		{
 			A[maxlen-i]=A[lenA-1-i];
 		}
-		for(i=0;i<maxlen+1-lenA)
+		for(i=0;i<maxlen+1-lenA;i++)
 		{
 			A[i]='0';
 		}
@@ -28,15 +22,23 @@ int main()
 		{
 			B[maxlen-i]=B[lenB-1-i];
 		}
-		for(i=0;i<maxlen+1-lenB)
+		for(i=0;i<maxlen+1-lenB;i++)
 		{
 			B[i]='0';
 		}
-		A[maxlen+1]=B[maxlen+1]=0;
+		A[maxlen+1]=0;
 		j=0;
-		for(i=maxlen;i>=0;i--)
+		for(i=0;i<maxlen;i++)
 		{
-			j+=A[]
+			j+=A[maxlen-i]+B[maxlen-i]-2*'0';
+			A[maxlen-i]=j%10+'0';
+			j/=10;
 		}
+		if(j!=0) printf("%c",j+'0');
+		if(*A=='0') printf("%s\n",A+1);
+		else printf("%s\n",A);
+		memset(A,'\0',sizeof(A));
+		memset(B,'\0',sizeof(B));
 	}
 }
+
